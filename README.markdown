fe52068024a91c90b5bdaeee5082aad47ded411c
Cody Robbins <cody@sakuzaku.com>
2011-05-31 14:55:14 -0400

    Added a readme and documentation for the model.

---
+++ README.markdown
@@ -0,0 +1,94 @@
+Static Model
+============
+
+This is a compilation of some boilerplate code for a non–database-backed model based on ActiveModel. You might want to use this kind of static model for anything that needs to be validated but doesn’t persist to the database—such as a login authenticator or an account password reset initiator. It includes the appropriate ActiveModel modules, has a constructor that initializes attributes from a hash, and has some helper methods useful for implementing custom validations.
+
+Full documentation is at [RubyDoc.info](http://rubydoc.info/gems/static-model).
+
+Example
+-------
+
+A user lookup class like the one below could be used as the parent class for a login authenticator or an account password reset initiator. It needs to find a user via their email, which entails validating the presence of the email attribute and providing access to the user that’s consequently looked up.
+
+    class UserLookup < StaticModel
+      # An accessor to allow specifying the email to look up.
+      attr_accessor(:email)
+
+      # A reader to allow access to the user that's looked up.
+      attr_reader(:user)
+
+      # Require the email attribute.
+      validates(:email,
+                :presence => {:message => 'Email is required.'})
+
+      # If the email is present, ensure that a user is associated with it.
+      validate do
+        no_other_errors? && user_exists?
+      end
+
+      # Find the user.
+      def user
+        User.find_by_email(@email)
+      end
+
+      # Add an error if no user is found.
+      def user_exists?
+        user ? true : add_email_error_and_return_false
+      end
+
+      protected
+
+      # Add an error.
+      def add_email_error_and_return_false
+        add_error_and_return_false('Email not found', :email)
+      end
+    end
+
+To use this class in a Rails controller, you’d do something like
+
+    class UserLookupController < ApplicationController
+      def find
+        create_user_lookup
+        do_something if email_valid?
+      end
+
+      protected
+
+      def create_user_lookup
+        @user_lookup = UserLookup.new(params[:user_lookup])
+      end
+
+      def do_something
+        @user_lookup.user.do_something
+      end
+
+      def email_valid?
+        request.post? && @user_lookup.valid?
+      end
+    end
+
+Colophon
+--------
+
+### See also
+
+If you like this gem, you may also want to check out [Declarative Find](http://codyrobbins.com/software/declarative-find), [Create New](http://codyrobbins.com/software/create-new), [Save Changes To](http://codyrobbins.com/software/save-changes-to), and [HTTP Error](http://codyrobbins.com/software/http-error).
+
+### Tested with
+
+* ActiveModel 3.0.5 — 20 May 2011
+
+### Contributing
+
+* [Source](https://github.com/codyrobbins/static-model)
+* [Bug reports](https://github.com/codyrobbins/static-model/issues)
+
+To send patches, please fork on GitHub and submit a pull request.
+
+### Credits
+
+© 2011 [Cody Robbins](http://codyrobbins.com/). See LICENSE for details.
+
+* [Homepage](http://codyrobbins.com/software/static-model)
+* [My other gems](http://codyrobbins.com/software#gems)
+* [Follow me on Twitter](http://twitter.com/codyrobbins)
--- lib/static-model.rb
+++ lib/static-model.rb
@@ -1,3 +1,68 @@
+# encoding: UTF-8
+
+# A compilation of some boilerplate code for a non–database-backed model based on ActiveModel. You might want to use this kind of static model for anything that needs to be validated but doesn’t persist to the database—such as a login authenticator or an account password reset initiator. It includes the appropriate ActiveModel modules, has a constructor that initializes attributes from a hash, and has some helper methods useful for implementing custom validations.
+#
+# Example
+# -------
+#
+# A user lookup class like the one below could be used as the parent class for a login authenticator or an account password reset initiator. It needs to find a user via their email, which entails validating the presence of the email attribute and providing access to the user that’s consequently looked up.
+#
+#     class UserLookup < StaticModel
+#       # An accessor to allow specifying the email to look up.
+#       attr_accessor(:email)
+#
+#       # A reader to allow access to the user that's looked up.
+#       attr_reader(:user)
+#
+#       # Require the email attribute.
+#       validates(:email,
+#                 :presence => {:message => 'Email is required.'})
+#
+#       # If the email is present, ensure that a user is associated with it.
+#       validate do
+#         no_other_errors? && user_exists?
+#       end
+#
+#       # Find the user.
+#       def user
+#         User.find_by_email(@email)
+#       end
+#
+#       # Add an error if no user is found.
+#       def user_exists?
+#         user ? true : add_email_error_and_return_false
+#       end
+#
+#       protected
+#
+#       # Add an error.
+#       def add_email_error_and_return_false
+#         add_error_and_return_false('Email not found', :email)
+#       end
+#     end
+#
+# To use this class in a Rails controller, you’d do something like
+#
+#     class UserLookupController < ApplicationController
+#       def find
+#         create_user_lookup
+#         do_something if email_valid?
+#       end
+#
+#       protected
+#
+#       def create_user_lookup
+#         @user_lookup = UserLookup.new(params[:user_lookup])
+#       end
+#
+#       def do_something
+#         @user_lookup.user.do_something
+#       end
+#
+#       def email_valid?
+#         request.post? && @user_lookup.valid?
+#       end
+#     end
 class StaticModel
   # Includes.
   include(ActiveModel::Naming)
@@ -6,7 +71,19 @@ class StaticModel
 
   # Methods.
 
-  # Initializes.
+  # Creates an instance of the model with its attributes initialized from a hash, just like you can do with ActiveRecord models.
+  #
+  # @param attributes [Hash] The attributes of the model and the respective values to initialize them with.
+  #
+  # @example
+  #    class User < StaticModel
+  #      attr_accessor(:name, :email)
+  #    end
+  #
+  #    user = User.new(:name => 'Dagny Taggart', :email => 'dagny@taggart.com')
+  #
+  #    user.name  #=> Dagny Taggart
+  #    user.email #=> dagny@taggart.com
   def initialize(attributes = {})
     attributes ||= {}
     attributes.each do |name, value|
@@ -14,19 +91,23 @@ class StaticModel
     end
   end
 
-  # This method simply tells Rails that this model is not database-backed.
+  # This method simply tells ActiveModel that this model is not database-backed.
   def persisted?
     false
   end
 
   protected
 
-  # Indicates whether there are currently any errors. Useful for validation methods that shouldn't fire if there are already other errors.
+  # A helper method to be used internally by subclasses which simply indicates whether there are currently any errors. This is useful for validation methods in subclasses that shouldn’t fire if there are already other errors. For example, the format of an email address might only need to be checked if the email address is present.
   def no_other_errors?
     errors.empty?
   end
 
-  # Adds an error to the `:base` attribute and returns false so that it can halt validations chains.
+  # A helper method to be used internally by subclasses which adds an error to the `:base` attribute and returns `false` so that it can halt validations chains.
+  #
+  # @param error [String] The error message.
+  # @param attribute [Symbol] The attribute of the model to place the error message on.
+  # @return [boolean] Returns `false` in order to halt any validation chain it may be invoked from.
   def add_error_and_return_false(error, attribute = :base)
     errors[attribute] << error
     false
